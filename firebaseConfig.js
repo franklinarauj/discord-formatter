40745b0d34ddde289cc6dabaa39207dfcd900ff0
Franklin Araujo <franklin.araujo@datainfo.inf.br>
2026-04-13 11:32:45 -0300

    Fix firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -11,5 +11,5 @@ const firebaseConfig = {
   measurementId: "G-QZ94YHBB4C"
 };
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
